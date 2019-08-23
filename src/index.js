d5a924f56d8f0c83d407f134c25127a6fe2ee2c8
Michael O'Donnell <gorillaejones@gmail.com>
2019-08-23 16:30:50 -0500

    initial setup looks to be really done this time

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import logger from 'redux-logger';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const fakeReducer = (state = '', action) => {
+    return state;
+}
+
+const store = createStore(
+    combineReducers({
+        fakeReducer
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
