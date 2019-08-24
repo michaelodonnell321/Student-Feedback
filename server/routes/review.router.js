7eeb806c6d0cafea7948f1721d46edf69eb630b1
Michael O'Donnell <gorillaejones@gmail.com>
2019-08-23 21:27:41 -0500

    post looks to be working on submit

---
+++ server/routes/review.router.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+// GET all reviews that have been placed
+router.get('/', (req, res) => {
+    // Return all reviews from feedback DB
+    pool.query('SELECT * FROM "feedback";').then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('Error GET /api/order', error);
+        res.sendStatus(500);
+    });
+})
+
+// POST a new order
+router.post('/', (req, res) => {
+    const newFeedback = req.body;
+    console.log(newFeedback);
+    const sqlText = `INSERT into feedback ("feeling", "understanding", "support", "comments")
+    VALUES ($1, $2, $3, $4);
+    `;
+    pool.query(sqlText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
+    .then((result) => {
+        console.log('back from DB post', result);
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        console.log('error in DB post', error);
+        res.sendStatus(500);
+    })
+})
+
+module.exports = router;
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import axios from 'axios';
 
 class Review extends Component {
+
+    clickHandler = () => {
+
+        let reviewObject = {
+            feeling: this.props.feedback[0],
+            understanding: this.props.feedback[1],
+            support: this.props.feedback[2],
+            comments: this.props.feedback[3],
+        }
+        axios.post('api/review', reviewObject)
+        .then(response => {
+            console.log('POST response is ', response);
+            this.props.dispatch({
+                type: 'CLEAR_REVIEW'
+            });
+            this.props.history.push('/');
+        })
+        .catch(error => {
+            console.log('error in PUT ', error);
+        });
+    };
+
     render() {
         return (
             <div>
-                
+                <h1>Review Your Feedback</h1>
+                {/*pull data from reduxStore using key in mapstate to props and position in array in order*/}
+                <h3>Feelings: {this.props.feedback[0]}</h3>
+                <h3>Understanding: {this.props.feedback[1]} </h3>
+                <h3>Support: {this.props.feedback[2]}</h3>
+                <h3>Comments: {this.props.feedback[3]}</h3>
+                <button onClick={this.clickHandler}>Submit</button>
             </div>
         );
     }
 }
 
-export default Review;
+const mapStateToProps = (reduxStore) => {
+    return {
+        feedback : reduxStore.formReducer
+    }
+}
+export default connect(mapStateToProps)(Review);
